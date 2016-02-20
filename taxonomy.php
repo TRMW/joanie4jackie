5063efa154c7fe91154e6c2da2bfb5b7f25c765c
Matt Wright (as J4J admin) <matt.c.wright@me.com>
2016-02-20 15:27:23 -0800

    Remove chainletter and intro links entirely
    Turns out the custom fields were deleted, so they're never be there,

--- taxonomy.php
+++ taxonomy.php
@@ -11,18 +11,7 @@
     <div class="entry-meta chainletter-meta">
       <?php the_chainletter_meta(); ?>
       <ul class="chainletter-links">
-        <?php
-          $term = get_queried_object();
-          $acg_id = $term->taxonomy._.$term->term_id;
-          $booklet_link = get_field('chainletter_booklet', $acg_id);
-          $intro_link = get_field('chainletter_intro', $acg_id);
-        ?>
-        <?php if ($booklet_link) : ?>
-          <li class="booklet-link"><a href="<?php echo $booklet_link; ?>" target="_blank">View booklet &raquo;</a></li>
-        <?php endif; ?>
-        <?php if ($intro_link) : ?>
-          <li><a href="<?php echo $intro_link; ?>" class="vimeo-link" target="_blank">Watch intro &raquo;</a></li>
-        <?php endif; ?>
+        <?php $term = get_queried_object(); $acg_id = $term->taxonomy._.$term->term_id; ?>
         <li><a href="/now/#<?php the_now_link($acg_id); ?>">Where are they now &raquo;</a></li>
       </ul>
       <?php if ( !empty($term->description) ) : ?>
